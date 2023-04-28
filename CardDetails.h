a9ebff3a79de28cafae4e94697a7edc278d7f9af
David <dkowalczyk135@gmail.com>
2023-04-28 19:40:47 -0400

    Create header file to store enumeration classes to represent Card class

---
+++ CardDetails.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+
+enum class Face {ACE = 1, TWO = 2, THREE = 3, FOUR = 4, 
+                FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, 
+                NINE = 9, TEN = 10, JACK = 11, QUEEN = 12, 
+                KING = 13};
+
+enum class Suit {SPADES, HEARTS, DIAMONDS, CLUBS};
