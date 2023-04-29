f67ced485b90b7d866f23971fa4a4d3a10cdf410
David <dkowalczyk135@gmail.com>
2023-04-28 22:36:29 -0400

    Add driver program to main()

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 
 #include <iostream>
-
+#include <iomanip>
+#include "CardDetails.h"
+#include "Card.h"
+#include "DeckOfCards.h"
 
 int main() {
+    DeckOfCards d1;
 
+    d1.shuffle();
+    int counter = 1;
+    while (counter <= 52) {
+        std::cout << std::setw(17) << std::left << d1.dealCard().toString() << " ";
+        if (counter % 4 == 0) {
+            std::cout << std::endl;
+        }
+        counter++;
+    }
 }
