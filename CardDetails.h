717928614fe075cd361cc79ff2b74ccd56070562
David <dkowalczyk135@gmail.com>
2023-04-28 20:58:15 -0400

    Make minor formatting fix

--- CardDetails.h
+++ CardDetails.h
@@ -1,10 +1,9 @@
 
 #pragma once
 
-
-enum class Face {ACE = 1, TWO = 2, THREE = 3, FOUR = 4, 
-                FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, 
-                NINE = 9, TEN = 10, JACK = 11, QUEEN = 12, 
-                KING = 13};
+enum class Face {ACE, TWO, THREE , FOUR, 
+                FIVE, SIX, SEVEN, EIGHT, 
+                NINE, TEN, JACK, QUEEN, 
+                KING};
 
 enum class Suit {SPADES, HEARTS, DIAMONDS, CLUBS};
