7e464064501a62e89b08224b59dbc56609cf1d21
David <dkowalczyk135@gmail.com>
2023-04-30 21:37:58 -0400

    Test functionality of PokerHand.cpp functions

--- main.cpp
+++ main.cpp
@@ -10,33 +10,7 @@
 void dealPokerHandsExperiment(int&);
 
 int main() {
-    DeckOfCards d1;
-    std::array<Card, 5> hand{};
-
-    // deal all 52 cards
-    // d1.shuffle();
-    // int counter = 1;
-    // while (counter <= 52) {
-    //     std::cout << std::setw(17) << std::left << d1.dealCard().toString() << " ";
-    //     if (counter % 4 == 0) {
-    //         std::cout << std::endl;
-    //     }
-    //     counter++;
-    // }
-    int tests = 10000;
-
-    // d1.shuffle();
-    // int cardCounter = 0;
-    // while (cardCounter < 5) {
-    //     hand[cardCounter++] = d1.dealCard();
-    // }
-
-    // for (Card card : hand) {
-    //     std::cout << std::setw(17) << std::left << card.toString() << " ";
-    // }
-    // std::cout << std::endl;
-
-    dealPokerHandsExperiment(tests);
+    
 }
 
 void dealPokerHandsExperiment(int& tests) {
@@ -72,9 +46,9 @@ void dealPokerHandsExperiment(int& tests) {
         counter++;
     }
 
-    std::cout << "Pairs occurred " << std::setprecision(2) << (static_cast<double>(pairs) / tests) * 100 << "\% of the time.\n";
-    std::cout << "Two Pairs occurred " << std::setprecision(2) << (static_cast<double>(twoPairs) / tests) * 100 << "\% of the time.\n";
-    std::cout << "Three of a kind occurred " << std::setprecision(2) << (static_cast<double>(threeOfAKind) / tests) * 100 << "\% of the time.\n";
-    std::cout << "Four of a kind occurred " << std::setprecision(2) << (static_cast<double>(fourOfAKind) / tests) * 100 << "\% of the time.\n";
-    std::cout << "Flush occurred " << std::setprecision(2) << (static_cast<double>(flush) / tests) * 100 << "\% of the time.\n";
+    std::cout << "Pairs occurred " << std::setprecision(3) << (static_cast<double>(pairs) / tests) * 100 << "\% of the time.\n";
+    std::cout << "Two Pairs occurred " << std::setprecision(3) << (static_cast<double>(twoPairs) / tests) * 100 << "\% of the time.\n";
+    std::cout << "Three of a kind occurred " << std::setprecision(3) << (static_cast<double>(threeOfAKind) / tests) * 100 << "\% of the time.\n";
+    std::cout << "Four of a kind occurred " << std::setprecision(3) << (static_cast<double>(fourOfAKind) / tests) * 100 << "\% of the time.\n";
+    std::cout << "Flush occurred " << std::setprecision(3) << (static_cast<double>(flush) / tests) * 100 << "\% of the time.\n";
 }
